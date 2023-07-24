4a2f2c1903654c9e549cf68b786c4cdc431e4f8a
debdut.saha <debdut.saha@angelbroking.com>
2023-07-24 15:57:12 +0530

    Added custom marker for the line chart.

--- app/src/main/java/com/example/chartexample/BarChartCombinedMarker.kt
+++ app/src/main/java/com/example/chartexample/BarChartCombinedMarker.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import android.graphics.Canvas
 import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import android.view.ViewGroup
 import android.widget.RelativeLayout
 import com.github.mikephil.charting.charts.Chart
 import com.github.mikephil.charting.components.IMarker
@@ -13,7 +13,7 @@ import com.github.mikephil.charting.highlight.Highlight
 import com.github.mikephil.charting.utils.MPPointF
 import java.lang.ref.WeakReference
 
-open class DottedLineMarkerView constructor(context: Context) :
+open class BarChartCombinedMarker constructor(context: Context) :
     RelativeLayout(context), IMarker {
 
     private val mOffset2 = MPPointF()
@@ -22,6 +22,7 @@ open class DottedLineMarkerView constructor(context: Context) :
     private var inflatedView: View? = null
 
     private var margin = Margin()
+
 
     fun setLayoutResource(res: Int) {
         setupLayoutResource(res)
@@ -33,9 +34,7 @@ open class DottedLineMarkerView constructor(context: Context) :
 
     override fun getOffset(): MPPointF {
         val barWidth = (chartView?.renderer as? RoundedBarChartRenderer)?.getIndividualBarWidth() ?: 0
-        val chartViewHighlight = chartView?.highlighted?.getOrNull(0)
-        val translate = if (chartViewHighlight?.dataSetIndex == 0) 1 else -1
-        return MPPointF(barWidth.div(2).times(translate).toFloat(),  0f)
+        return MPPointF(barWidth.div(2).toFloat(),  0f)
     }
 
     /**
@@ -45,14 +44,14 @@ open class DottedLineMarkerView constructor(context: Context) :
      */
     private fun setupLayoutResource(layoutResource: Int) {
         inflatedView = LayoutInflater.from(context).inflate(layoutResource, this)
-        inflatedView?.layoutParams = LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
+        inflatedView?.layoutParams = LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
         inflatedView?.measure(MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED), MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED))
         inflatedView?.layout(0, 0, inflatedView?.measuredWidth ?: 0, inflatedView?.measuredHeight ?: 0)
     }
 
     private fun forceLayoutChange() {
         val exactHeight = chartView?.viewPortHandler?.contentBottom()?.minus(margin.top) ?: 0f
-        inflatedView?.layoutParams = LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
+        inflatedView?.layoutParams = LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
         inflatedView?.measure(MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED), MeasureSpec.makeMeasureSpec(exactHeight.toInt(), MeasureSpec.EXACTLY))
         inflatedView?.layout(0, 0, inflatedView?.measuredWidth ?: 0, inflatedView?.measuredHeight ?: 0)
     }
--- app/src/main/java/com/example/chartexample/MultiLineChart.kt
+++ app/src/main/java/com/example/chartexample/MultiLineChart.kt
@@ -1,18 +1,12 @@
 package com.example.chartexample
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import android.util.AttributeSet
-import android.view.View
-import android.view.ViewGroup
+import android.util.Log
+import android.view.MotionEvent
 import android.widget.FrameLayout
-import androidx.core.content.ContextCompat
-import androidx.core.view.marginBottom
-import androidx.core.view.marginLeft
-import androidx.core.view.marginRight
-import androidx.core.view.marginTop
 import com.github.mikephil.charting.charts.LineChart
 import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.components.YAxis
@@ -20,16 +14,17 @@ import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
 import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
-
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Date
+import kotlin.math.abs
 
 class MultiLineChart @JvmOverloads constructor(
     private val context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
 ) : FrameLayout(context, attrs, defStyleAttr) {
-
-    // creating a string array for displaying days.
-    private val days = listOf("12PM", "1PM", "2PM", "3PM", "4PM", "5PM")
 
     init {
         // initializing variable for bar chart.
@@ -43,9 +38,7 @@ class MultiLineChart @JvmOverloads constructor(
         lineDataSet1.apply {
             setDrawValues(false)
             color = Color.parseColor("#F9BA4D")
-            lineWidth = 1.5f
             setCircleColor(Color.parseColor("#F9BA4D"))
-            circleRadius = 5f
             mode = LineDataSet.Mode.LINEAR
             setDrawValues(true)
             setDrawValues(false)
@@ -56,9 +49,7 @@ class MultiLineChart @JvmOverloads constructor(
         lineDataSet2.apply {
             setDrawValues(false)
             color = Color.parseColor("#581DBE")
-            lineWidth = 1.5f
             setCircleColor(Color.parseColor("#581DBE"))
-            circleRadius = 5f
             mode = LineDataSet.Mode.LINEAR
             setDrawValues(true)
             setDrawValues(false)
@@ -68,7 +59,6 @@ class MultiLineChart @JvmOverloads constructor(
         lineDataSet3.apply {
             setDrawValues(false)
             color = Color.parseColor("#581DBE")
-            lineWidth = 1.5f
             setCircleColor(Color.parseColor("#581DBE"))
             circleRadius = 5f
             mode = LineDataSet.Mode.LINEAR
@@ -81,7 +71,6 @@ class MultiLineChart @JvmOverloads constructor(
         lineDataSet4.apply {
             setDrawValues(false)
             color = Color.parseColor("#581DBE")
-            lineWidth = 1.5f
             setCircleColor(Color.parseColor("#581DBE"))
             circleRadius = 5f
             mode = LineDataSet.Mode.LINEAR
@@ -96,6 +85,7 @@ class MultiLineChart @JvmOverloads constructor(
         configureLineChart(lineChart = lineChart, lineData = lineData)
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     private fun configureLineChart(
         lineChart: LineChart,
         lineData: LineData,
@@ -104,16 +94,26 @@ class MultiLineChart @JvmOverloads constructor(
         lineChart.apply {
             data = lineData
             description.isEnabled = false
+            //isHighlightPerDragEnabled = false
+            //isHighlightPerTapEnabled = false
             xAxis.apply {
-                valueFormatter = IndexAxisValueFormatter(days)
+                valueFormatter = object : IndexAxisValueFormatter() {
+                    override fun getFormattedValue(value: Float): String {
+                        val simpleDateFormat = SimpleDateFormat("HH:mm aa")
+                        val calender = Calendar.getInstance()
+                        calender.time = Date(System.currentTimeMillis())
+                        calender.add(Calendar.MINUTE, value.toInt())
+                        return simpleDateFormat.format(calender.time)
+                    }
+                }
                 setCenterAxisLabels(true)
                 position = XAxis.XAxisPosition.BOTTOM
-                granularity = 1f
-                isGranularityEnabled = true
-                setDrawGridLines(false)
                 textColor = Color.parseColor("#ACB2BD")
                 textSize = 12f
                 axisLineColor = Color.parseColor("#ACB2BD")
+                axisMinimum = 0f
+                axisMaximum = 30f
+                setLabelCount(6, true)
             }
             axisLeft.apply {
                 setDrawGridLines(false)
@@ -127,57 +127,102 @@ class MultiLineChart @JvmOverloads constructor(
                 textSize = 12f
                 axisLineColor = Color.parseColor("#ACB2BD")
             }
-
             isDragEnabled = true
-            setVisibleXRangeMaximum(4f)
             setScaleEnabled(false)
             legend.isEnabled = false
+            extraBottomOffset = 20f
+            setOnTouchListener { _, motionEvent ->
+                when (motionEvent.action) {
+                    MotionEvent.ACTION_DOWN -> {
+                        if (lineChart.viewPortHandler.isInBoundsX(motionEvent.rawX)) {
+                            val tapPointOnChart = lineChart.getTransformer(YAxis.AxisDependency.LEFT).getValuesByTouchPoint(motionEvent.rawX,motionEvent.rawY)
+                            val values = buildList {
+                                repeat(lineData.dataSetCount) {
+                                    add(nearestValue(value = tapPointOnChart.x.toInt(), data = lineData.getDataSetByIndex(it)))
+                                }
+                            }
+                            setOrUpdateMarker(lineChart = lineChart, rawX = motionEvent.rawX, data = values, time = lineChart.xAxis.valueFormatter.getFormattedValue(tapPointOnChart.x.toFloat()))
+                            Log.d("CHARTS", "Values: $values - ${values.count()}")
+                        }
+                    }
+                }
+                true
+            }
             scope()
             invalidate()
         }
     }
 
+    private fun nearestValue(value: Int, data: ILineDataSet): MultiLineData? {
+        if (data.entryCount == 0) return null
+        if (data.entryCount == 1) return MultiLineData(entry = data.getEntryForIndex(0), color = data.color) // (10) (12) (17) (19) (23) -- 15
+        var nearValue = data.getEntryForIndex(0)
+        for (index in 1 until data.entryCount) {
+            val currentNearXValue = abs(nearValue?.x?.minus(value) ?: 0f)
+            val localNearXValue = abs(data.getEntryForIndex(index).x.minus(value))
+            if (localNearXValue < currentNearXValue) {
+                nearValue = data.getEntryForIndex(index)
+            }
+        }
+        return MultiLineData(entry = nearValue, color = data.color)
+    }
+
     private fun getEntriesOne(): List<Entry> {
         return listOf(
-            Entry(0f, 18000f),
-            Entry(1f, 17600f),
-            Entry(2f, 16500f),
-            Entry(3f, 17600f),
-            Entry(4f, 17650f),
-            Entry(5f, 17500f)
+            Entry(0f, 17000f),
+            Entry(8f, 17050f),
+            Entry(12f, 17120f),
+            Entry(16f, 17300f),
+            Entry(18f, 17400f),
+            Entry(20f, 17200f)
         )
     }
 
     private fun getEntriesTwo(): List<Entry> {
         return listOf(
             Entry(0f, 7f),
-            Entry(1f, 6f),
-            Entry(2f, 8f),
-            Entry(3f, 0f),
-            Entry(4f, 8f),
-            Entry(5f, 7f)
+            Entry(7f, 6f),
+            Entry(13f, 10f),
+            Entry(16f, 12f),
+            Entry(19f, 16f),
+            Entry(23f, 20f)
         )
     }
 
     private fun getEntriesThree(): List<Entry> {
         return listOf(
             Entry(0f, 0f),
-            Entry(1f, 10f),
-            Entry(2f, 15f),
-            Entry(3f, 12f),
-            Entry(4f, 20f),
-            Entry(5f, 8f),
+            Entry(6f, 10f),
+            Entry(10f, 15f),
+            Entry(12f, 12f),
+            Entry(18f, 20f),
+            Entry(26f, 8f)
         )
     }
 
     private fun getEntriesFour(): List<Entry> {
         return listOf(
             Entry(0f, 0f),
-            Entry(1f, 18f),
-            Entry(2f, 5f),
-            Entry(3f, 20f),
-            Entry(4f, 5f),
-            Entry(5f, 20f)
+            Entry(4f, 18f),
+            Entry(8f, 5f),
+            Entry(14f, 20f),
+            Entry(18f, 5f),
+            Entry(30f, 20f)
         )
+    }
+
+    private fun setOrUpdateMarker(lineChart: LineChart, rawX: Float, data: List<MultiLineData?>, time: String) {
+        val view = if (childCount == 2) {
+            getChildAt(1) as? MultiLineChartMarkerView
+        } else {
+            val requiredHeightForTheView = lineChart.viewPortHandler.contentBottom()
+            Log.d("CHARTS", "Required Height: $requiredHeightForTheView")
+            val view = MultiLineChartMarkerView(context)
+            val params = LayoutParams(LayoutParams.WRAP_CONTENT, requiredHeightForTheView.toInt())
+            addView(view, params)
+            view
+        }
+        view?.x = rawX
+        view?.setData(data = data, time = time)
     }
 }
